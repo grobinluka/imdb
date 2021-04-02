b5fbee5a654f621c0111dcf8318674254268fbf4
grobinluka <grobinluka@gmail.com>
2021-04-02 10:54:14 +0200

    Start of Registration

--- header.php
+++ header.php
@@ -10,11 +10,11 @@
     <title>Boljše kot IMDB</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="./dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    <link href="./css/blog.css" rel="stylesheet">
+    <link href="css/blog.css" rel="stylesheet">
   </head>
 
   <body>
@@ -32,7 +32,8 @@
             <a class="text-muted" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
             </a>
-            <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+            <a class="btn btn-sm btn-outline-secondary" href="user_add.php">Registracija</a>
+            <a class="btn btn-sm btn-outline-secondary" href="login.php">Prijava</a>
           </div>
         </div>
       </header>
