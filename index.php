3aadfbd0a1d24edda4de3aaf6c8f7bae08ca8a5b
grobinluka <grobinluka@gmail.com>
2021-04-02 10:26:08 +0200

    Added Design

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    include_once "header.php";
+?>
+    <h1>Dobrodošli na strani INDEX</h1>
+    <?php 
+
+    $a = 1876;
+
+    if (isset($_GET['id'])) {
+        $b = $_GET['id'];
+    }
+    else {
+        $b=0;
+    }
+
+    $b++;
+
+    echo $b;
+
+    echo '<a href="movies.php?vrednost='.$b.'">MOVIES</a>';
+
+    ?>
+<?php
+    include_once "footer.php";
+?>
