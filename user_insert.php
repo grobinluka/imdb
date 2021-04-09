82cfea23fabb4e44a0055b88f61b755a2464db34
grobinluka <grobinluka@gmail.com>
2021-04-09 09:15:08 +0200

    Registration Added

--- user_insert.php
+++ user_insert.php
@@ -1,5 +1,22 @@
 <?php 
 
-print_r($_POST)
+include_once "db.php";
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$pass1 = $_POST['pass1'];
+$pass2 = $_POST['pass2'];
+
+//preverim ali so podatki polni
+if(!empty($first_name) && !empty($last_name) && !empty($email) && !empty($pass1) && ($pass1 == $pass2)){
+    //vnos v bazo
+    $pass = password_hash($pass1, PASSWORD_DEFAULT);
+    $query = "INSERT INTO users(first_name, last_name, email, pass) VALUES(?,?,?,?)";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$first_name, $last_name, $email, $pass]);
+}
+
+//nazaj na registracijo
 
 ?>
